a0f9faaaaf99fe116486ded8f00a7c9e1876a919
Abelardo J. Marquez <ajmarquezg@gmail.com>
2015-08-26 00:17:31 -0430

    Initial Commit

---
+++ WTTF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WTTF
+//
+//  Created by Abelardo Marquez on 8/26/15.
+//  Copyright (c) 2015 ajmg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WTTF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WTTF
+//
+//  Created by Abelardo Marquez on 8/26/15.
+//  Copyright (c) 2015 ajmg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
